8e595ddb845c611ee25a036584b5f1a8b555771c
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-11-02 08:00:57 +0000

    added cout statements for information mpg

--- .ccls-cache/@home@runner@HW10P6/main.cpp
+++ .ccls-cache/@home@runner@HW10P6/main.cpp
@@ -12,6 +12,7 @@ int GallonsUsed;
 int main() {
   const int NumCars = 5;
   car cars[NumCars];
+      cout << "Enter car number, miles driven, and gallons used. Each separated by a space and new line after entering each gallon amount." << endl;
   for (int i = 0; i < NumCars; i++){
     cin >> cars[i].CarNumber;
     cin >> cars[i].MilesDriven;
@@ -22,11 +23,10 @@ double totalMPG = 0;
 for(int i = 0; i < NumCars; i++){
   double mpg = cars[i].MilesDriven / cars[i].GallonsUsed;
   totalMPG += mpg;
-
-  
+  cout << "Car number " << cars[i].CarNumber << " used " << mpg << " miles per gallon." << endl;
 }
-  
+cout << "Average mpg for all cars: " << totalMPG << endl;
+
+  return 0;
 }
-
-
 
